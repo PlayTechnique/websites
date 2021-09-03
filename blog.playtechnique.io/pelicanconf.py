9193b028a66099501bf7e2543c4fa7f906fe1953
Gwyn <gwyn@playtechnique.io>
2021-09-03 11:03:18 -0600

    Slight gitignore and pelicanconf improvements.

--- blog.playtechnique.io/pelicanconf.py
+++ blog.playtechnique.io/pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = 'lovely gwyn'
-SITENAME = 'lives'
+SITENAME = 'normal human life'
 SITEURL = ''
 
 PATH = 'content'
@@ -19,10 +19,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
